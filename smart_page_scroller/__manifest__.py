ad250487823948f6500b1bad1bc02407d0cb0a9c
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-07-26 18:22:51 +0530

    [ADD] Initial commit for SmartPageScroller for branch 10.0

--- smart_page_scroller/__manifest__.py
+++ smart_page_scroller/__manifest__.py
@@ -11,7 +11,7 @@
                 'website', 'product'
                 ],
     'category': 'Website',
-    'version': '11.0.1.0.0',
+    'version': '10.0.1.0.0',
     'description': """
     -- in website product page scroll top buttton apply.
     """,
