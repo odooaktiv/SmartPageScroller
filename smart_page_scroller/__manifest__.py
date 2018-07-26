c1ff06a3e023262c979095afebce16b634cf93aa
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-07-26 18:32:40 +0530

    [IMP] Manifest website SmartPageScroller for branch 10.0

--- smart_page_scroller/__manifest__.py
+++ smart_page_scroller/__manifest__.py
@@ -1,14 +1,12 @@
-#  -*- coding: utf-8 -*-
-# Part of AktivSoftware See LICENSE file for full
-# copyright and licensing details.
+# -*- encoding: utf-8 -*-
 {
     'name': 'smart page scroller',
     'summary': 'product page in scroll button apply',
     'author': "Aktiv Software",
     'website': "http://www.aktivsoftware.com",
     'depends': ['website_sale',
-                'sale_management',
-                'website', 'product'
+                'website', 'product',
+                'sale'
                 ],
     'category': 'Website',
     'version': '10.0.1.0.0',
